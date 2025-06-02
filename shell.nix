abdac98b181cab8b3822f025088156593311c9dc
Aurora Dawn <aurora.dawn@trans.fish>
2025-06-02 15:01:43 -0400

    Shelled!

---
+++ shell.nix
@@ -0,0 +1,20 @@
+let
+  nixpkgsVer = "10d7f8d34e5eb9c0f9a0485186c1ca691d2c5922";
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/${nixpkgsVer}.tar.gz") { config = {}; overlays = []; };
+  libs = with pkgs; [
+  ];
+in pkgs.mkShell {
+  name = "caelis";
+
+  buildInputs = libs ++ (with pkgs; [
+    cargo
+    rustc
+    gcc
+    rustfmt
+    pkg-config
+  ]);
+
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  RUST_BACKTRACE = 1;
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libs;
+}
